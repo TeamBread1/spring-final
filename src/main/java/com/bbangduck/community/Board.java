61498c603d237d731bc5282df70f51089e3c9cc8
SungSuu <tjdtnzlffj@gmail.com>
2021-08-17 19:21:14 +0900

    좋아요 스프링

--- src/main/java/com/bbangduck/community/Board.java
+++ src/main/java/com/bbangduck/community/Board.java
@@ -1,11 +1,23 @@
 package com.bbangduck.community;
 
+import java.util.HashSet;
+import java.util.Set;
+
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToMany;
+
+
+import com.bbangduck.heart.Heart;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,12 +25,43 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
+@Builder
 public class Board {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String title;
-	private String content;
+	private String postTitle;
+	
+	@Lob
+	@Column(length=100000)
+	private String postContent;
+	
+	@Column(nullable = false)
+	private String postPwd;
+	
+	@Column(nullable = false, length = 20)
+	private String postDate;
+	
+	@Column(nullable = false)
+	private String postAuthor;
+	
+
+	
+	@JsonIgnore
+    @OneToMany
+    @Builder.Default
+    private Set<Heart> hearts = new HashSet<>();
+	
+	  public void addHeart(Heart heart){
+	        this.hearts.add(heart);
+	        heart.setBoard(this);
+	    }
+
+	    public void deleteHeart(Heart heart){
+	        this.hearts.remove(heart);
+	        heart.setBoard(null);
+	    }
+	
 }
