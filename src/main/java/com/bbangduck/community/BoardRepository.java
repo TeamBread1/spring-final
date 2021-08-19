b4eb0dcbcbf9218e9b30e233a2419b656bbf4926
Junyoung Kim <rlawnsdud521@gmail.com>
2021-08-19 23:25:41 +0900

    update api

--- src/main/java/com/bbangduck/community/BoardRepository.java
+++ src/main/java/com/bbangduck/community/BoardRepository.java
@@ -18,13 +18,8 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
 	@Query(value = "SELECT id, post_title as postTitle, post_content as postContent, post_date as postDate, post_author as postAuthor, post_image as postImage, post_like as postLike, post_pwd as postPwd FROM board WHERE id = :id", nativeQuery = true)
 	Optional<BoardWithoutComment> findByIdOnlyBoard(@Param("id") Long id);
-//	@Query(value = "SELECT b FROM board b", nativeQuery = true)
-//	List<Board> findAllData();
-//	@Query("select b from Board b left join fetch b.comment")
-//	List<Board> findAllWithCommentList();
 
 	@Query(value = "SELECT id as id, post_title as postTitle, post_content as postContent, post_image as postImage FROM board order by post_like desc limit 6", nativeQuery = true)
 	List<Best6Post> findBest6Post();
 
-//	@Query(value = "SELECT id, post_title as postTitle, post_content as postContent, post_date as postDate, post_author as postAuthor, post_image as postImage, post_like as postLike, post_pwd as postPwd FROM board WHERE ", nativeQuery = true)
 }
